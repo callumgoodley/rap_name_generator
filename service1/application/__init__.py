50dda887cb6b929545b2adbf635710baf02166f7
callum <callumgoodley@btinternet.com>
2020-07-08 12:11:10 +0000

    mysql container connected and functional

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + str(getenv('DB_USERNAME'))+ ':' + str(getenv('DB_PASSWORD')) + '@rap_name_generator_mysql_1:3306/rapper-name-db'
 app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
+
+db = SQLAlchemy(app)
 
 from application import routes
--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
-from application import app
+from application import app, db
 from application.forms import NameForm
+from application.models import Users
 import requests
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -24,5 +25,8 @@ def home():
 
         name = requests.post('http://service4:5003', data=code_string )
 
+        postData = Users( first_name=first_name, last_name=last_name, rapper_name=name.text)
+        db.session.add(postData)
+        db.session.commit()
 
     return render_template('home.html', title='Home', form = form,  code = code_string, name = name)
