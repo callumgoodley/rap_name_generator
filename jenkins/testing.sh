27c2ad90f4f566911915893caaabf162adc31620
callum <callumgoodley@btinternet.com>
2020-07-09 11:59:11 +0000

    venv activation added to testing.sh

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+
+. /home/jenkins/.jenkins/workspace/rap-name-generator/venv/bin/activate
+
 export MY_SECRET_KEY='aeiou'
-export DB_USERNAME='root'
+export DB_USERNAME='callumgoodley'
 export DB_PASSWORD='root'
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/
