b2258477c4ac51dc276af3242cda7748396489e9
callum <callumgoodley@btinternet.com>
2020-07-09 10:49:50 +0000

    changed command in testing.sh from 'python -m pytest' to just 'pytest'

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -6,6 +6,6 @@ sudo docker stop mysql-test
 sudo docker rm mysql-test
 sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_DATABASE=test
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
-python3 -m pytest
+pytest
 sudo docker stop mysql-test
 sudo docker rm mysql-test
