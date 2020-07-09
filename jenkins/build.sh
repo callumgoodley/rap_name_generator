80664b8255f63068067a8803463f6fcf3bc2561c
callum <callumgoodley@btinternet.com>
2020-07-09 14:23:07 +0000

    venv activation and env var's added back into build.sh as build is failing without

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-  
+
+. /home/jenkins/.jenkins/workspace/rap-name-generator/venv/bin/activate
+
+export MY_SECRET_KEY=$MY_SECRET_KEY
+export DB_USERNAME=$DB_USERNAME
+export DB_PASSWORD=$DB_PASSWORD
+
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/
 
 sudo docker-compose build --parallel
