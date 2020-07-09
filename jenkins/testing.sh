81530e6fef3112a3da146aeee384aef5f9be633d
callum <callumgoodley@btinternet.com>
2020-07-09 10:45:32 +0000

    testing commands for jenkins file moved to seperate script

---
+++ jenkins/testing.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+cd /home/jenkins/.jenkins/workspace/rap-name-generator/
+
+sudo docker stop mysql-test
+sudo docker rm mysql-test
+sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_DATABASE=test
+cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
+python3 -m pytest
+sudo docker stop mysql-test
+sudo docker rm mysql-test
