71fed032fb0ad1ed668cffe7c35dc18fcfdbd39a
callum <callumgoodley@btinternet.com>
2020-07-09 13:54:14 +0000

    --cov corrected typo's

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -12,10 +12,10 @@ cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
 python -m pytest --cov application
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service2/testing
-python -m pytest -cov application
+python -m pytest --cov application
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service3/testing
-python -m pytest -cov application
+python -m pytest --cov application
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service4/testing
-python -m pytest -cov application
+python -m pytest --cov application
