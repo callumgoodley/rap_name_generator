be78ab7798c3fc966c2343829bd6e923fe2d20b6
callum <callumgoodley@btinternet.com>
2020-07-09 13:13:48 +0000

    echo for env var's

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -8,6 +8,10 @@ export DB_PASSWORD='root'
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/
 
+echo $MY_SECRET_KEY
+echo $DB_USERNAME
+echo $DB_PASSWORD
+
 sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_USER='callumgoodley' MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' -e MYSQL_DATABASE=test
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
 python -m pytest
