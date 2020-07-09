bf4136a294596563ed3110ea4c0610b64871b213
callum <callumgoodley@btinternet.com>
2020-07-09 13:19:35 +0000

    docker ps added ot script

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -13,6 +13,9 @@ echo $DB_USERNAME
 echo $DB_PASSWORD
 
 sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_USER='callumgoodley' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' -e MYSQL_DATABASE=test
+
+sudo docker ps
+
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
 python -m pytest
 sudo docker stop mysql-test
