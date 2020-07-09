b4ce26f4745aa66ce421c7ffcf70a75abe71c7d2
callum <callumgoodley@btinternet.com>
2020-07-09 13:27:09 +0000

    mysql container on local machine now

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -8,15 +8,5 @@ export DB_PASSWORD='root'
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/
 
-echo $MY_SECRET_KEY
-echo $DB_USERNAME
-echo $DB_PASSWORD
-
-sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_USER='callumgoodley' -e MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' -e MYSQL_DATABASE=test
-
-sudo docker ps
-
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
 python -m pytest
-sudo docker stop mysql-test
-sudo docker rm mysql-test
