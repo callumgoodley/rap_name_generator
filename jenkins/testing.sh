79d5e0533c62dacb5bafe3a452d92feab44405e1
callum <callumgoodley@btinternet.com>
2020-07-09 11:40:23 +0000

    user created for test db

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -7,7 +7,7 @@ cd /home/jenkins/.jenkins/workspace/rap-name-generator/
 
 sudo docker stop mysql-test
 sudo docker rm mysql-test
-sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_ROOT_PASSWORD='root' -e MYSQL_DATABASE=test
+sudo docker run -d -p 3306:3306 --name mysql-test mysql -e MYSQL_USER='callumgoodley' MYSQL_PASSWORD='root' -e MYSQL_ROOT_PASSWORD='root' -e MYSQL_DATABASE=test
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/service1/testing
 python -m pytest
 sudo docker stop mysql-test
