7e0dd0c6092cd143f6b6b47928415adb45446f61
callum <callumgoodley@btinternet.com>
2020-07-10 15:34:46 +0000

    script

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
-
-
-
-
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
