8d4cd4e3e9184dda1736ffe68a213442eca8a6a4
callum <callumgoodley@btinternet.com>
2020-07-10 12:58:12 +0000

    install ansible in script

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/configure
-
-/usr/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
+sudo apt-get install ansible
+ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
