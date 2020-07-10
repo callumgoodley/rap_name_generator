2ccedd6187c0cb73d284ccbd6d6cea6d9f19fcbd
callum <callumgoodley@btinternet.com>
2020-07-10 15:27:04 +0000

    deploy script

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 
-cp /home/jenkins/.local/bin/ansible-playbook /home/jenkins/usr/bin/ansible-playbook
-whoami
-#/home/callumgoodley/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
-/home/jenkins/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
+
+
+/home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
