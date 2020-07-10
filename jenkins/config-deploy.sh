95156ed0b1543e84def28d451b8d1dfdc64662be
callum <callumgoodley@btinternet.com>
2020-07-10 14:38:21 +0000

    link added to deploy script for ansible playbook

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 
-/home/callumgoodley/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
-#/home/jenkins/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
+ln -s /home/jenkins/.local/bin/ansible-playbook /usr/bin/ansible-playbook
+#/home/callumgoodley/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
+/home/jenkins/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
