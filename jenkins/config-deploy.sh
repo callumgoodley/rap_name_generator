608677c3533859779608db76a4670ad06e33fc99
callum <callumgoodley@btinternet.com>
2020-07-10 13:52:16 +0000

    script changed for deploy

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cd /home/jenkins/.jenkins/workspace/rap-name-generator/configure
 
-/home/jenkins/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
+/home/callumgoodley/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
+#/home/jenkins/.local/bin/ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
