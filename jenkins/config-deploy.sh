606fa57b6c4b37acfc266d252d9da4c0277347ed
callum <callumgoodley@btinternet.com>
2020-07-10 13:03:21 +0000

    spacing

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/configure
+
 sudo apt-get install ansible
+
 ansible-playbook -v -i 34.105.252.67, --inventory=inventory playbook.yaml
