d59d11b7d3eedec9e77a9da8e2415a47bb64cd7c
callum <callumgoodley@btinternet.com>
2020-07-11 18:50:18 +0000

    docker login added to script

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -6,6 +6,7 @@ export MY_SECRET_KEY=$MY_SECRET_KEY
 export DB_USERNAME=$DB_USERNAME
 export DB_PASSWORD=$DB_PASSWORD
 
+sudo docker login -u $DH_USERNAME -p $DH_PASSWORD docker.io
 sudo docker rmi $(sudo docker images -aq)
 sudo docker-compose build --parallel
 sudo docker-compose push
