3d7a7cbc97031188b053d1f1d0e50bab6e2f26d8
callum <callumgoodley@btinternet.com>
2020-07-11 18:28:32 +0000

    build script pushes added

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -12,8 +12,8 @@ sudo docker-compose build --parallel
 #sudo docker-compose push
 
 
-sudo docker push callumgoodley/service1 + ":$BUILD_NUMBER"
-sudo docker push callumgoodley/service2 + ":$BUILD_NUMBER"
-sudo docker push callumgoodley/service3 + ":$BUILD_NUMBER"
-sudo docker push callumgoodley/service4 + ":$BUILD_NUMBER"
-sudo docker push callumgoodley/rapper_name_mysql + ":$BUILD_NUMBER"
+sudo docker push '${callumgoodley/service1}":$BUILD_NUMBER"'
+#sudo docker push callumgoodley/service2 + ":$BUILD_NUMBER"
+#sudo docker push callumgoodley/service3 + ":$BUILD_NUMBER"
+#sudo docker push callumgoodley/service4 + ":$BUILD_NUMBER"
+#sudo docker push callumgoodley/rapper_name_mysql + ":$BUILD_NUMBER"
