49bbbe7b51b29a1a4d763d2167d82618b38287f5
callum <callumgoodley@btinternet.com>
2020-07-11 19:10:59 +0000

    pushing to docker hub without build number

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -6,14 +6,14 @@ export MY_SECRET_KEY=$MY_SECRET_KEY
 export DB_USERNAME=$DB_USERNAME
 export DB_PASSWORD=$DB_PASSWORD
 
-sudo docker login -u $DH_USERNAME -p $DH_PASSWORD docker.io
+sudo docker login
 sudo docker rmi $(sudo docker images -aq)
 sudo docker-compose build --parallel
 sudo docker-compose push
 
 
-
-#sudo docker push callumgoodley/service2 + ":$BUILD_NUMBER"
-#sudo docker push callumgoodley/service3 + ":$BUILD_NUMBER"
-#sudo docker push callumgoodley/service4 + ":$BUILD_NUMBER"
-#sudo docker push callumgoodley/rapper_name_mysql + ":$BUILD_NUMBER"
+#sudo docker push "callumgoodley/service1:$BUILD_NUMBER"
+#sudo docker push "callumgoodley/service2:$BUILD_NUMBER"
+#sudo docker push "callumgoodley/service3:$BUILD_NUMBER"
+#sudo docker push "callumgoodley/service4:$BUILD_NUMBER"
+#sudo docker push "callumgoodley/rapper_name_mysql:$BUILD_NUMBER"
