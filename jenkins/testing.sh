81cf937442384fd283ef6045706c17de1edfbed0
callum <callumgoodley@btinternet.com>
2020-07-12 10:04:09 +0000

    env variables removed from testing script

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-. /home/jenkins/.jenkins/workspace/rap-name-generator/venv/bin/activate
+#. /home/jenkins/.jenkins/workspace/rap-name-generator/venv/bin/activate
 
-export MY_SECRET_KEY='aeiou'
-export DB_USERNAME='callumgoodley'
-export DB_PASSWORD='root'
+#export MY_SECRET_KEY='aeiou'
+#export DB_USERNAME='callumgoodley'
+#export DB_PASSWORD='root'
 
 cd /home/jenkins/.jenkins/workspace/rap-name-generator/
 
