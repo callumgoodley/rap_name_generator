ce5391207dbfe1a39e381fa493032340105a3ad7
callum <callumgoodley@btinternet.com>
2020-07-12 19:46:34 +0000

    changed testing routes again

--- jenkins/testing.sh
+++ jenkins/testing.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-cd /home/callumgoodley/rap_name_generator/service1/testing
+cd ./service1/testing
 python -m pytest --cov application
-cd /home/callumgoodley/rap_name_generator/service2/testing
+
+cd ./service2/testing
 python -m pytest --cov application
-cd /home/callumgoodley/rap_name_generator/service3/testing
+
+cd ./service3/testing
 python -m pytest --cov application
-cd /home/callumgoodley/rap_name_generator/service4/testing
+
+cd ./service4/testing
 python -m pytest --cov application
