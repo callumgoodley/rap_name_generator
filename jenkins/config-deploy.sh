83c0c37ca53bc8913935492a0a33f5e9903cd677
callum <callumgoodley@btinternet.com>
2020-07-12 21:09:37 +0000

    replica commands added individual ssh

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
 ssh callumgoodley@rap-manager << EOF
-sudo docker service update -–replicas 3 rapstack_service1
-sudo docker service update -–replicas 3 rapstack_service2
-sudo docker service update -–replicas 3 rapstack_service3
-sudo docker service update -–replicas 3 rapstack_service4
 sudo docker service update --replicas 3 rapstack_mysql
+EOF
+
+ssh callumgoodley@rap-manager << EOF
+sudo docker service update -–replicas 3 rapstack_service1
+EOF
+ssh callumgoodley@rap-manager << EOF
+sudo docker service update -–replicas 3 rapstack_service2
+EOF
+ssh callumgoodley@rap-manager << EOF
+sudo docker service update -–replicas 3 rapstack_service3
+EOF
+ssh callumgoodley@rap-manager << EOF
+sudo docker service update -–replicas 3 rapstack_service4
 EOF
