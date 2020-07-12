a2f7d3b8290a4a891411d8fe30da2a731ba4fc2e
callum <callumgoodley@btinternet.com>
2020-07-12 21:39:39 +0000

    nginx put back in docker-compose file

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
 ssh callumgoodley@rap-manager << EOF
-sudo docker service scale rapstack_mysql=3 rapstack_service1=3 rapstack_service2=3 rapstack_service3=3 rapstack_service4=3
+sudo docker service scale rapstack_mysql=5 rapstack_service1=5 rapstack_service2=5 rapstack_service3=5 rapstack_service4=5
 EOF
 
