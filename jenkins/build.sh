e6b72898015446f2d7ba606514c6d83ee1f9b5d4
callum <callumgoodley@btinternet.com>
2020-07-12 11:43:25 +0000

    added to build script for failure to test if email notification is working

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,10 +3,10 @@
 sudo docker login
 sudo docker rmi $(sudo docker images -aq)
 sudo docker-compose build --parallel
-sudo docker-compose push
+#sudo docker-compose push
 
 
-#sudo docker push "callumgoodley/service1:$BUILD_NUMBER"
+sudo docker push "callumgoodley/service1:$BUILD_NUMBER"
 #sudo docker push "callumgoodley/service2:$BUILD_NUMBER"
 #sudo docker push "callumgoodley/service3:$BUILD_NUMBER"
 #sudo docker push "callumgoodley/service4:$BUILD_NUMBER"
