85496a3ba63e8edc504e6695280b152efd91a770
callum <callumgoodley@btinternet.com>
2020-07-12 13:04:44 +0000

    build.sh changes to remove purposeful error

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,11 +3,5 @@
 sudo docker login
 sudo docker rmi $(sudo docker images -aq)
 sudo docker-compose build --parallel
-#sudo docker-compose push
+sudo docker-compose push
 
-
-sudo docker push "callumgoodley/service1:$BUILD_NUMBER"
-#sudo docker push "callumgoodley/service2:$BUILD_NUMBER"
-#sudo docker push "callumgoodley/service3:$BUILD_NUMBER"
-#sudo docker push "callumgoodley/service4:$BUILD_NUMBER"
-#sudo docker push "callumgoodley/rapper_name_mysql:$BUILD_NUMBER"
