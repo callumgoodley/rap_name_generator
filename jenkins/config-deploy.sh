dab5441616779d027f2e0f27ca474bcf8de8d49f
callum <callumgoodley@btinternet.com>
2020-07-12 20:58:37 +0000

    moved from main to script

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
+ssh callumgoodley@rap-manager << EOF
+sudo docker service update -–replicas 3 rapstack_service1
+sudo docker service update -–replicas 3 rapstack_service2
+sudo docker service update -–replicas 3 rapstack_service3
+sudo docker service update -–replicas 3 rapstack_service4
+sudo docker service update --replicas 3 rapstack_mysql
+EOF
