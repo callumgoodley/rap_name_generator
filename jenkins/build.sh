53d4d3d70610267531baebdbb55256f0e5a62e88
callum <callumgoodley@btinternet.com>
2020-07-12 09:59:42 +0000

    env variables removed from build script

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-. /home/jenkins/.jenkins/workspace/rap-name-generator/venv/bin/activate
-
-export MY_SECRET_KEY=$MY_SECRET_KEY
-export DB_USERNAME=$DB_USERNAME
-export DB_PASSWORD=$DB_PASSWORD
-
 sudo docker login
 sudo docker rmi $(sudo docker images -aq)
 sudo docker-compose build --parallel
