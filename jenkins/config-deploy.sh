4b57521f547a725796f4b8997ea4cd93730dd2f8
callum <callumgoodley@btinternet.com>
2020-07-12 21:19:15 +0000

    all in one ssh command increase replicas

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,18 +1,6 @@
 #! /bin/bash
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
 ssh callumgoodley@rap-manager << EOF
-sudo docker service update --replicas 3 rapstack_mysql
+sudo docker service update rapstack_mysql=3 rapstack_service1=3 rapstack_service2=3 rapstack_service3=3 rapstack_service4=3
 EOF
 
-ssh callumgoodley@rap-manager << EOF
-sudo docker service update -–replicas 3 rapstack_service1
-EOF
-ssh callumgoodley@rap-manager << EOF
-sudo docker service update -–replicas 3 rapstack_service2
-EOF
-ssh callumgoodley@rap-manager << EOF
-sudo docker service update -–replicas 3 rapstack_service3
-EOF
-ssh callumgoodley@rap-manager << EOF
-sudo docker service update -–replicas 3 rapstack_service4
-EOF
