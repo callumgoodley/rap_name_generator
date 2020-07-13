28a9e4094cba32ac88e7d1c0a917aeff507e2cd2
callum <callumgoodley@btinternet.com>
2020-07-13 16:16:48 +0000

    config-deploy script changed

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 ssh callumgoodley@rap-manager3 << EOF
-sudo docker container stop $(docker container ls –aq)
-sudo docker container rm $(docker container ls –aq)
-docker system prune
+sudo docker system prune
 EOF
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
 
