c5d7b792a3ac318cdb596520f977138df9ddc418
callum <callumgoodley@btinternet.com>
2020-07-13 15:42:03 +0000

    changed rap manager

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+ssh callumgoodley@rap-manager3 << EOF
+sudo docker container stop $(docker container ls –aq)
+sudo docker container rm $(docker container ls –aq)
+docker system prune
+EOF
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
-ssh callumgoodley@rap-manager2 << EOF
+ssh callumgoodley@rap-manager3 << EOF
 sudo docker service scale rapstack_nginx=3 rapstack_mysql=5 rapstack_service1=5 rapstack_service2=5 rapstack_service3=5 rapstack_service4=5
 EOF
 
