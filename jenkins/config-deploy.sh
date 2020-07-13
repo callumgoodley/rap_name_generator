ae39d21bd8db490d7d2e81e2af77e71de710279d
callum <callumgoodley@btinternet.com>
2020-07-13 16:10:33 +0000

    changes for replicas added

--- jenkins/config-deploy.sh
+++ jenkins/config-deploy.sh
@@ -5,7 +5,4 @@ sudo docker container rm $(docker container ls –aq)
 docker system prune
 EOF
 /home/jenkins/.local/bin/ansible-playbook -i /home/callumgoodley/rap_name_generator/configure/inventory /home/callumgoodley/rap_name_generator/configure/playbook.yaml
-ssh callumgoodley@rap-manager3 << EOF
-sudo docker service scale rapstack_nginx=3 rapstack_mysql=5 rapstack_service1=5 rapstack_service2=5 rapstack_service3=5 rapstack_service4=5
-EOF
 
