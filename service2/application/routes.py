e1679009f32fce3a34927f877049e860d332c0c2
callum <callumgoodley@btinternet.com>
2020-07-13 12:25:06 +0000

    manager vm changed

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -1,11 +1,11 @@
 from flask import Response, request
 from application import app
 
-def generate_number(name):
+def generate_number(first_name):
         unicode_sum = 0
-        for letter in name:
+        for letter in first_name:
             unicode_sum += ord(letter)
-        unicode_num_first = unicode_sum/len(name)
+        unicode_num_first = unicode_sum/len(first_name)
         int_num = int(unicode_num_first)
         number = str(unicode_num_first)
         return number
